673bda0f08f190032da0c7c0ae1306af9bad3e9c
lukdoerr <doerr_lukas@gmx.de>
2023-05-09 12:21:42 +0200

    Quicksort Standard and M3 finished

--- Rust/quick/src/main.rs
+++ Rust/quick/src/main.rs
@@ -1,30 +1,102 @@
 use std::fs;
+use std::env;
 
-fn quicksort(z: &mut Vec<i8>, p: usize, r: usize)
+fn quicksort(z: &mut Vec<i8>, p: usize, r: usize, m: usize)
 {
     if p < r
     { 
-        let q: usize = partition(z, p, r);
-        quicksort(z, p, q-1);
-        quicksort(z, q+1, r);
+        let q: usize;
+        if m==0
+        {
+            q = partition(z, p, r) as usize;
+        }
+        else 
+        {
+            q = fastpartition(z, p, r) as usize;
+        }
+        
+        if q<1 {return;}
+
+        quicksort(z, p, q-1, m);
+        quicksort(z, q+1, r, m);
     }
 }
 
-fn partition(z: &mut Vec<i8>, p: usize, r: usize) -> usize
-{   
+
+
+fn vglpartion(z: &mut Vec<i8>, l: usize, r: usize) -> usize
+{
+    let m = (l + r) / 2;
+    return m;
+}
+
+
+
+
+fn fastpartition(z: &mut Vec<i8>, l: usize, r: usize) -> i64
+{
+    let m = (l + r) / 2;
+    
+    println!("L({l})\tM({m})\tR({r})"); 
+    println!("L({})\tM({})\tR({})\n", z[l], z[m], z[r]); 
+    
+    // Left Element is bigger than middle
+    if z[m] < z[l]
+    {
+        // Move left Element to middle
+        println!("Move Left to Middle"); 
+        println!("L({l}):{}\tM({m}):{}\tR({r}):{}", z[l], z[m], z[r]); 
+        swap(z, m, l);
+        
+        println!("L({l}):{}\tM({m}):{}\tR({r}):{}\n", z[l], z[m], z[r]); 
+       
+    }
+
+    // Left is bigger than right 
+    if z[r] < z[l]
+    {
+        // Move left Elemnet to right 
+        println!("Move Left to Right"); 
+        println!("L({l}):{}\tM({m}):{}\tR({r}):{}", z[l], z[m], z[r]); 
+        swap(z, r, l);
+        
+        println!("L({l}):{}\tM({m}):{}\tR({r}):{}\n", z[l], z[m], z[r]); 
+    }
+
+    // Middle is bigger than right
+    if z[r] < z[m]
+    {
+        // Move middle to right
+        println!("Move Middle to Right"); 
+        println!("L({l}):{}\tM({m}):{}\tR({r}):{}", z[l], z[m], z[r]); 
+        swap(z, r, m);
+        println!("L({l}):{}\tM({m}):{}\tR({r}):{}\n", z[l], z[m], z[r]); 
+    }
+    println!("\n\n");
+
+    let r1 = r - 1;
+
+    swap(z, m, r1);
+
+    return partition(z, l, r1);
+}
+
+
+fn partition(z: &mut Vec<i8>, p: usize, r: usize) -> i64
+{
     let pivot = z[r];
-    let mut i = p - 1;
+    let mut i: i64 = p as i64 - 1;
 
     for j in p..r
     {
         if z[j] <= pivot
         {
             i = i+1;
-            swap(z, i, j);
+            swap(z, i as usize, j);
 
         }
     }
-    swap(z, i+1, r);
+    swap(z, (i+1) as usize, r);
     return i+1;
 }
 
@@ -57,9 +129,13 @@ fn main() {
 
     let timer = Instant::now();
     let mut times: Vec<std::time::Duration> = Vec::new();
-    
+   
+    let args: Vec<String> = env::args().collect();
+    let s = args[1].to_string().parse::<usize>().unwrap();
+        
+
     // Go through all Sequences
-    for i in 0..4
+    for i in 0..1
     {
         // read Data from Sequence
         let mut content: Vec<i8> =  read_seq(i);
@@ -71,7 +147,7 @@ fn main() {
 
         // Start algorithm
         let start = timer.elapsed();
-        quicksort(&mut content, 1, n-1);
+        quicksort(&mut content, 0, n-1, s);
         let end  = timer.elapsed();
 
         // Store needed time for analytics
--- low-level/bitwizardry/bitw.c
+++ low-level/bitwizardry/bitw.c
@@ -19,19 +19,19 @@ void printBits(size_t const size, void const * const ptr)
 }
 
 
-int addBit(int b)
-{
-   
-}
-
 int main(int argc, char* argv[])
 {
-    uint8_t bit = 0;
+    uint8_t bit = 1;
 
-    for(int i=0; i<20; i++)
+   // for(int i=0; i<20; i++)
     {
-        uint8_t x = ~bit;
-        printBits(1, &x);
+
+        uint8_t x = ~(bit | (bit>>1));
+        uint8_t y =  x & ~(x-1);
+        uint8_t z = bit & ~bit;
+        printf("%8b\n", x, x);
+        printf("%8b\n", y, y);
+        printf("%8b\n", z, z);
     }
     return 0;
 }
